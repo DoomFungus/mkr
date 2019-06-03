1bd7a6bd54c99a33efc3cff020505c9b409eb68b
DoomFungus <unspeakableferretofdoom@gmail.com>
2019-06-03 21:14:05 +0300

    Initial commit

---
+++ src/main/java/edu/kpi/java/mkr/MkrApplication.java
@@ -0,0 +1,13 @@
+package edu.kpi.java.mkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkrApplication.class, args);
+    }
+
+}
